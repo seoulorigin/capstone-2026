d609484d4a27cc020b446663b8501fd9dfd14ce7
4tae <118651121+4tae@users.noreply.github.com>
2026-03-28 15:01:44 +0900

    chore: MSW mock 환경 설정 (#29)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,22 @@ import "./index.css"
 
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* React Query 전역 Provider 설정 */}
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
-)
+async function enableMocking() {
+  if (import.meta.env.DEV) {
+    const { worker } = await import("./mocks/browser")
+
+    await worker.start({
+      onUnhandledRequest: "bypass",
+    })
+  }
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  )
+})
