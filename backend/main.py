2a1d9e5729d8f1a865927ba2bfb61dcf02b323d6
seoulorigin <seoulorigin@gmail.com>
2026-03-29 16:01:59 +0900

    chore: CORS EC2 public IP 추가

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173"],
+        "http://localhost:5173",
+        "http://localhost:5173",
+        "http://52.78.113.234:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
